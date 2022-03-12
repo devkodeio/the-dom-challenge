47656284225e5dc1346f306a3525a197a22bb21d
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2022-03-12 13:06:14 +0530

    Added img

--- star-rating/script.js
+++ star-rating/script.js
@@ -1,15 +1,36 @@
-const star = document.querySelectorAll('.star');
+const star = document.querySelectorAll(".star");
 
-star.forEach(function(item,index) {
-    item.addEventListener('click', function (e) {
-        if (item.classList.contains("empty")) {
-            for(let i=0;i<=index;i++){
-                star[i].classList = "star filled show";
-            }
-        } else {
-            for(let i= 4;i>index;i--){
-                star[i].classList = "star empty show"
-            }
+star.forEach(function(item, index) {
+  item.addEventListener("mouseover", function() {
+    for (let i = 0; i <= index; i++) {
+      star[i].classList = "star filled show";
+    }
+    item.addEventListener("click", function(e) {
+      if (item.classList.contains("empty")) {
+        for (let i = 0; i <= index; i++) {
+          star[i].classList = "star filled show";
         }
+      } else {
+        for (let i = 4; i > index; i--) {
+          star[i].classList = "star empty show";
+        }
+      }
+      item.removeEventListener("click", function(e) {
+            if (item.classList.contains("empty")) {
+              for (let i = 0; i <= index; i++) {
+                star[i].classList = "star filled show";
+              }
+            } else {
+              for (let i = 4; i > index; i--) {
+                star[i].classList = "star empty show";
+              }
+            }
+      })
     });
+  };
+  item.onmouseout = function() {
+    for (let i = 0; i <= index; i++) {
+      star[i].classList = "star empty show";
+    }
+  };
 });
