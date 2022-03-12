a06dee5b9eac3843ac902b3cc727307e700a8664
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2022-03-12 15:27:34 +0530

    Added Featues

--- pixel-art/script.js
+++ pixel-art/script.js
@@ -1,7 +1,6 @@
 let grid = document.querySelector('#grid');
 let rows=null;
-let color=["background-color:#0c98ca","background-color:#7200cf","background-color:#f43100","background-color:#00d6d8","background-color:#200052","background-color:#60e800","background-color:#6d00ff","background-color:#ff7c55","background-color:#00a650","background-color:#00a9cd"]
-
+let selectedColor = 
 function pixelArt(el,rows,columns){
     let griditem="";
     for(let i=0;i<rows;i++){
@@ -12,12 +11,17 @@ function pixelArt(el,rows,columns){
     el.innerHTML = griditem;
     rows = document.querySelectorAll('.grid-row');
     rows[9].innerHTML= colorCols(columns);
+    let colors = rows[9].querySelectorAll(".cell");
+
 }
 
 function colorCols(col) {
     let str="";
     for (let i = 0; i < col; i++) {
-        str+=`<div class="grid-col cell" style=${color[i]}></div>`
+        let a= Math.floor(Math.random() * 256); 
+        let b= Math.floor(Math.random() * 256); 
+        let c= Math.floor(Math.random() * 256); 
+        str+=`<div class="grid-col cell" style="background-color:rgb(${a},${b},${c}); cursor:pointer"></div>`
     }
     return str
 }
@@ -28,5 +32,7 @@ function cols(griditem,columns){
     }
     return griditem;
 }
+
+
 
 pixelArt(grid,10,10);
