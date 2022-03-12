358347b827e7e546f199d9a475a2cc0f5f288812
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2022-03-12 13:22:01 +0530

    Add

--- star-rating/script.js
+++ star-rating/script.js
@@ -5,32 +5,19 @@ star.forEach(function(item, index) {
     for (let i = 0; i <= index; i++) {
       star[i].classList = "star filled show";
     }
-    item.addEventListener("click", function(e) {
-      if (item.classList.contains("empty")) {
-        for (let i = 0; i <= index; i++) {
-          star[i].classList = "star filled show";
-        }
-      } else {
-        for (let i = 4; i > index; i--) {
-          star[i].classList = "star empty show";
-        }
-      }
-      item.removeEventListener("click", function(e) {
-            if (item.classList.contains("empty")) {
-              for (let i = 0; i <= index; i++) {
-                star[i].classList = "star filled show";
-              }
-            } else {
-              for (let i = 4; i > index; i--) {
-                star[i].classList = "star empty show";
-              }
-            }
-      })
-    });
-  };
-  item.onmouseout = function() {
-    for (let i = 0; i <= index; i++) {
+    for(let i=index+1;i<=4;i++){
       star[i].classList = "star empty show";
     }
-  };
+  });
+  item.addEventListener("click", function(e) {
+    if (item.classList.contains("empty")) {
+      for (let i = 0; i <= index; i++) {
+        star[i].classList = "star filled show";
+      }
+    } else {
+      for (let i = 4; i > index; i--) {
+        star[i].classList = "star empty show";
+      }
+    }
+  });
 });
